28b290b2076753f5923b6f2595aaf4629b6a1675
ArthurFloro <arthur.floro2006@gmail.com>
2026-07-13 14:47:15 -0300

    feat(forum): cria caso de uso para buscar question por slug
    - Implementa o caso de uso focado em recuperar uma pergunta (Question) específica através do seu slug.
    - Atualiza a interface do repositório para suportar a busca utilizando essa propriedade única.
    - Adiciona testes unitários para validar o comportamento da busca e garantir a integridade da funcionalidade.

--- src/domain/forum/application/repositories/questions-repository.ts
+++ src/domain/forum/application/repositories/questions-repository.ts
@@ -2,5 +2,6 @@ import type { Question } from "../../enterprise/entities/questions";
 
 
 export interface QuestionsRepository {
+  findBySlug(slug: string): Promise<Question | null>
   create(question: Question): Promise<void>
 }
---
+++ src/domain/forum/application/use-cases/get-question-by-slug.spec.ts
@@ -0,0 +1,35 @@
+import { beforeEach, describe, expect, it } from "vitest"
+import { InMemoryQuestionsRepository } from "../../../../../test/in-memory-questions-repository"
+import { Question } from "../../enterprise/entities/questions"
+import { UniqueEntityID } from "../../../../core/entities/unique-entity-id"
+import { GetQuestionBySlugUseCase } from "./get-question-by-slug"
+import { Slug } from "../../../value-objects/slug"
+
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: GetQuestionBySlugUseCase
+
+describe('Get Question By Slug', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new GetQuestionBySlugUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('should be able to get a question by slug', async () => {
+    const newQuestion = Question.create({
+      authorId: new UniqueEntityID(),
+      title: 'Example Question',
+      slug: Slug.create('example-question'),
+      content: 'Example content',
+    })
+
+    await inMemoryQuestionsRepository.create(newQuestion)
+
+    const { question } = await sut.execute({
+      slug: 'example-question',
+    })
+
+    expect(question.id).toBeTruthy()
+    expect(question.title).toEqual(newQuestion.title)
+  })
+})
