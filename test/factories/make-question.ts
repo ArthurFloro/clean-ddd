cfa437de341b94ae526ff7e505205e7546a8c8a9
ArthurFloro <arthur.floro2006@gmail.com>
2026-07-15 08:02:25 -0300

    test: integra @faker-js/faker à factory de question
    - Adiciona o pacote @faker-js/faker para geração automatizada de dados fictícios.
    - Atualiza a factory makeQuestion para utilizar dados aleatórios em suas propriedades padrão.
    - Aumenta a cobertura e a confiabilidade dos testes permitindo a simulação de diversos cenários com dados dinâmicos.

--- test/factories/make-question.ts
+++ test/factories/make-question.ts
@@ -1,13 +1,15 @@
+import { faker } from "@faker-js/faker";
 import { UniqueEntityID } from "../../src/core/entities/unique-entity-id";
 import { Question, type QuestionProps } from "../../src/domain/forum/enterprise/entities/questions";
 
-export function makeQuestion(override: Partial<QuestionProps> = {}) {
+export function makeQuestion(override: Partial<QuestionProps> = {}, id?: UniqueEntityID) {
+
   const question = Question.create({
     authorId: new UniqueEntityID(),
-    title: 'Example',
-    content: 'Example content',
+    title: faker.lorem.sentence(),
+    content: faker.lorem.text(),
     ...override
-  })
+  }, id)
 
   return question
 }
