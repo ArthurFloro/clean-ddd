eb2861a4c41d23e10287a6dccfe32b7c89da5de3
ArthurFloro <arthur.floro2006@gmail.com>
2026-07-15 12:03:41 -0300

    feat(forum): cria caso de uso e testes para comentar em answer
    - Implementa o caso de uso responsável por adicionar comentários (AnswerComment) a uma resposta (Answer) existente.
    - Configura a lógica de instanciação da entidade e a integração com a interface do repositório.
    - Cria testes unitários para validar o fluxo de criação e garantir a integridade da nova funcionalidade.

---
+++ test/repositories/in-memory-answer-comments.ts
@@ -0,0 +1,11 @@
+import type { AnswerCommentsRepository } from "../../src/domain/forum/application/repositories/answer-comments-repositories"
+import type { AnswerComment } from "../../src/domain/forum/enterprise/entities/answer-comment"
+
+export class InMemoryAnswerCommentsRepository
+  implements AnswerCommentsRepository {
+  public items: AnswerComment[] = []
+
+  async create(answerComment: AnswerComment) {
+    this.items.push(answerComment)
+  }
+}
