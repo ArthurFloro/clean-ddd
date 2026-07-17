5d1a7b2e5f9819fec6d8bc7642365c2bc92101f4
ArthurFloro <arthur.floro2006@gmail.com>
2026-07-17 12:47:46 -0300

    feat(core): cria estrutura base para domain events
    - Implementa as interfaces e classes base para a criação, disparo e escuta de eventos de domínio.
    - Configura o core de entidades e adapta a classe de agregados (AggregateRoot) para suportar o registro e armazenamento temporário de eventos.
    - Prepara a arquitetura da aplicação para possibilitar a comunicação reativa e desacoplada entre diferentes contextos (Bounded Contexts).

---
+++ src/domain/forum/enterprise/entities/question-attachment-list.ts
@@ -0,0 +1,10 @@
+import type { QuestionAttachment } from "./question-attachment";
+import { WatchedList } from "./watched-list";
+
+
+
+export class QuestionAttachmentList extends WatchedList<QuestionAttachment> {
+  compareItems(a: QuestionAttachment, b: QuestionAttachment): boolean {
+    return a.attachmentId.equals(b.attachmentId)
+  }
+}
