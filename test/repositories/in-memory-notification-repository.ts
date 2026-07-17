862baa72f9a5ddd25e31a4d1420a7570ee43df4d
ArthurFloro <arthur.floro2006@gmail.com>
2026-07-17 12:12:16 -0300

    feat(notifications): cria caso de uso para envio de notificação
    - Implementa o caso de uso responsável por orquestrar a criação e o envio de novas notificações.
    - Cria o repositório em memória (InMemoryNotificationsRepository) para simular a persistência de dados.
    - Adiciona testes unitários para validar a lógica de criação e garantir o funcionamento correto do fluxo.

---
+++ test/repositories/in-memory-notification-repository.ts
@@ -0,0 +1,11 @@
+import type { NotificationsRepository } from "../../src/domain/notification/application/repositories/notification-repository"
+import { Notification } from "../../src/domain/notification/enterprise/entities/notification"
+
+export class InMemoryNotificationsRepository
+  implements NotificationsRepository {
+  public items: Notification[] = []
+
+  async create(notification: Notification) {
+    this.items.push(notification)
+  }
+}
